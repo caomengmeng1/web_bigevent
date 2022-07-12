3124bea0437983f4d93770888ae38ca29d96a46f
bcmgit <m18713320514@163.com>
2022-07-12 14:08:43 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,31 @@ $.ajaxPrefilter(function(options) {
     // console.log(options.url);
     options.url = 'http://www.liulongbin.top:3007' + options.url;
     // console.log(options.url);
+
+
+    // 统一为有权限的接口，设置headers请求头
+    /*   options.headers = {
+          Authorization: localStorage.getItem('token') || ''
+      } */
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 全局统一挂载 complete回调函数
+    options.complete = function(res) {
+        // console.log('执行了complete函数');
+        // console.log(res);
+        // 在complete函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 01.强制清空 token
+            localStorage.removeItem('token');
+            //  02.强制跳转到登录页面
+            location.href = '/login.html';
+        }
+
+    }
 })
